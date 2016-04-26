0f4ccb33ecb4d165f87d206c86abcd1bf8a84f83
shkschneider <shk.schneider@gmail.com>
2016-04-26 15:03:32 +0200

    SystemUI: long press recent switchs to last app (3/3)

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -200,6 +200,7 @@ public class KeyButtonView extends ImageView {
                     }
                 }
                 removeCallbacks(mCheckLongPress);
+                mPerformedLongClick = false;
                 break;
         }
 
